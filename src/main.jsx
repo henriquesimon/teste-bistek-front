f387b45c2bc7f595603e84304436d598cdb558f3
henriquesimon <henriquesimon1998@gmail.com>
2024-03-17 13:58:38 -0300

    Feat: add shadcn and products-form screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
+import { ProductsForm } from "./screens/products-form";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ProductsForm />
   </React.StrictMode>
 );
